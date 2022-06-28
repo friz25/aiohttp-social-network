304ea59635ceb3c0d3d6c404e3f7b630db88846a
frizmob@gmail.com <friz48625>
2022-06-28 14:52:49 +0300

    v2

--- handlers/base.py
+++ handlers/base.py
@@ -1,14 +1,33 @@
+from datetime import datetime
+
 from aiohttp import web
+from aiohttp_session import get_session
 
-async def index(request):
+class Index(web.View):
     """ Главная страница """
-    conf = request.app['config']
-    return web.Response(text = 'Hello Aiohttp!')
+    async def get(self):
+        """ [GET] Главная страница """
+        conf = self.app['config']
+        return web.Response(text = 'Hello Aiohttp!')
 
-async def login(request):
+class Login(web.View):
     """ Страница Вход в аккаунт """
-    return web.Response(text = 'login Aiohttp!')
+    async def get(self):
+        """ [GET] Страница Вход в аккаунт \n
+        : session : хранит datetime последнего посещения"""
+        session = await get_session(self)
+        session['last_visit'] = str(datetime.utcnow())
+        last_visit = session['last_visit']
+        text = f'Last visited: {last_visit}'
+        return web.Response(text = f'login Aiohttp!, {text}')
 
-async def signup(request):
+    async def post(self):
+        """ [POST] Страница Вход в аккаунт """
+        return web.Response(text = 'login Aiohttp!')
+
+class Signup(web.View):
     """ Страница Регистрация """
-    return web.Response(text = 'signup Aiohttp!')
+    async def get(self):
+        """ [GET] Страница Регистрация """
+
+        return web.Response(text = 'signup Aiohttp!')
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+import base64 # v2
 import logging
 from aiohttp import web
+
+from cryptography import fernet # v2
+from aiohttp_session import setup # v2
+from aiohttp_session.cookie_storage import EncryptedCookieStorage # v2
 
 from routes.base import setup_routes
 from config.common import BaseConfig
 
 def main():
     app = web.Application()
+
+    """ Генерируем ключ aiohttp-сессии \n
+    теперь у каждого пользователя (зашедшего на сайт) будет храниться свой coockie-ключ - идентификатор"""
+    fernet_key = fernet.Fernet.generate_key() # v2
+    secret_key = base64.urlsafe_b64decode(fernet_key) # v2
+    setup(app, EncryptedCookieStorage(secret_key)) # v2
+
     setup_routes(app)
     app['config'] = BaseConfig
     logging.basicConfig(level=logging.DEBUG)
